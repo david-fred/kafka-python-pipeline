483f0c3da3ffc342240d4866826277af50c355f0
MacBook <freddavidc@gmail.com>
2026-06-08 11:55:36 +0100

    feat: stream live weather data to local Kafka topic using QuixStreams
    - Initialize a QuixStreams Application configured for a local Kafka broker at localhost:9092
    - Fetch current 2m temperature data for London from the Open-Meteo API
    - Set up a Kafka producer using a context manager to ensure safe connection handling
    - Serialize the weather payload to JSON and produce it to the 'weather_data_demo' topic

--- send_to_kafka/main.py
+++ send_to_kafka/main.py
@@ -1,7 +1,22 @@
 import requests
-response = requests.get("https://api.open-meteo.com/v1/forecast", params={
-    "latitude": 51.5,
-    "longtitude": -0.11,
-    "current": "temperature_2m",
-})
-print(response.json())
+import json
+from quixstreams import Application
+response = requests.get(
+    "https://api.open-meteo.com/v1/forecast", params={
+        "latitude": 51.5,
+        "longitude": -0.11,
+        "current": "temperature_2m",
+    },
+)
+weather = response.json()
+
+app = Application(
+    broker_address="localhost:9092",
+    loglevel="DEBUG",
+)
+with app.get_producer() as producer:
+    producer.produce(
+        topic="weather_data_demo",
+        key="London",
+        value=json.dumps(weather),
+    )
